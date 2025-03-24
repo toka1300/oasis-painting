8f4f54f5ccd175d0fc7bf5049d1562049a8f1096
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2025-03-24 16:31:59 -0400

    Initial commit

---
+++ src/_data/client.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "name": "New Growth Web Services",
+    "email": "newgrowthweb@gmail.com",
+    "phoneForTel": "613-857-9828",
+    "phoneFormatted": "(613) 857-9828",
+    "address": {
+        "lineOne": "123 Fake Street",
+        "city": "Ottawa",
+        "province": "ON",
+        "postalCode": "K0A1X0",
+        "country": "Canada",
+        "mapLink": "https://maps.app.goo.gl/5y6iGEJqFwFWFt9i9"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.example.com"
+};
