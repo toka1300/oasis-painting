d9e864f00f77e2d906f7676da155ad5867e71ea2
toka1300 <caseytokarchuk@gmail.com>
2025-03-30 11:08:47 -0400

    add contact page

--- src/_data/client.js
+++ src/_data/client.js
@@ -3,6 +3,7 @@ module.exports = {
     "email": "manager@oasispainting.ca",
     "phoneForTel": "613-795-6277",
     "phoneFormatted": "(613) 795-6277",
+    "bookingLink": "https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ0-NZtHZtxLqQVzw9gMcsMjfCfJnus7J4yRayl7mDdVxe9bXFZzKw9dRLnnldQZ0bFS3wqZ9En8?gv=true",
     "socials": {
         "facebook": "https://www.facebook.com/oasispaintingcan/",
         "instagram": "https://www.instagram.com/oasispainting",
