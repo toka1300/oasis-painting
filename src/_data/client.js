6ccf7463f0e19734a3b49777777df2a6e4b31f4e
toka1300 <caseytokarchuk@gmail.com>
2025-03-31 16:22:03 -0400

    desktop qa

--- src/_data/client.js
+++ src/_data/client.js
@@ -4,6 +4,7 @@ module.exports = {
     "phoneForTel": "613-795-6277",
     "phoneFormatted": "(613) 795-6277",
     "bookingLink": "https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ0-NZtHZtxLqQVzw9gMcsMjfCfJnus7J4yRayl7mDdVxe9bXFZzKw9dRLnnldQZ0bFS3wqZ9En8?gv=true",
+    "mapLink" :"https://maps.app.goo.gl/U7kL7jNRF3DDiyjr5",
     "socials": {
         "facebook": "https://www.facebook.com/oasispaintingcan/",
         "instagram": "https://www.instagram.com/oasispainting",
