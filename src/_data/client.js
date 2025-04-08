cc8d3990bd06d89721416aca55ecae80792d68da
toka1300 <caseytokarchuk@gmail.com>
2025-04-08 08:31:24 -0400

    fix phone number to Will

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,8 +1,8 @@
 module.exports = {
     "name": "Oasis Painting",
     "email": "manager@oasispainting.ca",
-    "phoneForTel": "613-795-6277",
-    "phoneFormatted": "(613) 795-6277",
+    "phoneForTel": "613-408-4432",
+    "phoneFormatted": "(613) 408-4432",
     "bookingLink": "https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ0-NZtHZtxLqQVzw9gMcsMjfCfJnus7J4yRayl7mDdVxe9bXFZzKw9dRLnnldQZ0bFS3wqZ9En8?gv=true",
     "mapLink" :"https://maps.app.goo.gl/U7kL7jNRF3DDiyjr5",
     "socials": {
